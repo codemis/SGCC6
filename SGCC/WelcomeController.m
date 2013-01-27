496bdf42bdfd2591af4f4c0f330e4df9b41d9423
Codemis <johnathan@missionaldigerati.com>
2013-01-26 16:33:01 -0800

    Added Action Sheet for Contact Info.

--- SGCC/WelcomeController.m
+++ SGCC/WelcomeController.m
@@ -8,8 +8,9 @@
 
 #import "WelcomeController.h"
 #import "WebsiteViewController.h"
-
-@interface WelcomeController ()
+#define SGUC_PHONE @"tel://16262870486"
+@interface WelcomeController () <UIActionSheetDelegate>
+- (IBAction)contactButtonClicked:(id)sender;
 @end
 
 @implementation WelcomeController
@@ -18,7 +19,8 @@
 {
 }
 
--(void) prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+-(void) prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
+{
     if ([[segue identifier] isEqualToString:@"whoIsJesusSegue"]) {
         WebsiteViewController *controller = [segue destinationViewController];
         controller.websiteURL = [NSURL URLWithString:@"http://www.matthiasmedia.com.au/2wtl/2wtlonline.html"];
@@ -26,4 +28,37 @@
     }
 }
 
+- (IBAction)contactButtonClicked:(id)sender
+{
+    UIActionSheet *actionSheet = [[UIActionSheet alloc] initWithTitle:@"Contact Us"
+                                                             delegate:self
+                                                    cancelButtonTitle:@"Cancel"
+                                               destructiveButtonTitle:nil
+                                                    otherButtonTitles:  @"Call Us",
+                                  @"Email Us",
+                                  @"Directions",
+                                  @"Visit Website",
+                                  nil];
+    [actionSheet showFromTabBar:self.tabBarController.tabBar];
+}
+#pragma mark - UIActionSheetDelegate Functions
+-(void)     actionSheet:(UIActionSheet *)actionSheet
+   clickedButtonAtIndex:(NSInteger)buttonIndex
+{
+    switch (buttonIndex) {
+        case 0:
+            //Call
+            [[UIApplication sharedApplication] openURL:[NSURL URLWithString:SGUC_PHONE]];
+            break;
+        case 1:
+            //Email
+            break;
+        case 2:
+            //Directions
+            break;
+        case 3:
+            //Website
+            break;
+    }
+}
 @end
