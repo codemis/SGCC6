ebd3dad332b8177c6406282704104f1ba1f242e0
Bruce Hobbs <brucehobbs@engineeredsw.com>
2013-09-24 15:13:42 -0700

    Cosmetic changes

--- SGCC/TabBarControllerDelegate.h
+++ SGCC/TabBarControllerDelegate.h
@@ -1,13 +1,3 @@
-//
-//  TabBarControllerDelegate.h
-//  SGCC
-//
-//  Created by Bruce Hobbs on 8/13/13.
-//  Copyright (c) 2013 Johnathan Pulos. All rights reserved.
-//
-
-#import <Foundation/Foundation.h>
-
 @interface TabBarControllerDelegate : NSObject
 
 @end
