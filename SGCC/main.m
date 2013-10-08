f7c70fcdc612c36220f82e2ecf0f9820061c0915
Bruce Hobbs <brucehobbs@engineeredsw.com>
2013-10-08 16:23:16 -0700

    Badging complete

--- SGCC/main.m
+++ SGCC/main.m
@@ -1,13 +1,3 @@
-//
-//  main.m
-//  SGCC
-//
-//  Created by Johnathan Pulos on 1/3/13.
-//  Copyright (c) 2013 Johnathan Pulos. All rights reserved.
-//
-
-#import <UIKit/UIKit.h>
-
 #import "AppDelegate.h"
 
 int main(int argc, char *argv[])
