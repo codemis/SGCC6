1772658109932b98a08c18494257af273c24179b
Bruce Hobbs <brucehobbs@engineeredsw.com>
2014-04-29 15:16:07 -0700

    Created download task

--- SGCC/SermonsTableViewController.m
+++ SGCC/SermonsTableViewController.m
@@ -2,6 +2,7 @@
 #import "SermonsDataSource.h"
 #import "Sermon.h"
 #import "AudioPlayerViewController.h"
+#import <AFNetworking.h>
 
 @interface SermonsTableViewController () <UITableViewDelegate>
 @end
@@ -16,7 +17,19 @@
 #pragma mark - UITableViewDelegate
 -(void)                        tableView:(UITableView *)tableView
 accessoryButtonTappedForRowWithIndexPath:(NSIndexPath *)indexPath {
+    NSURLSessionConfiguration *configuration = [NSURLSessionConfiguration defaultSessionConfiguration];
+    AFURLSessionManager *manager = [[AFURLSessionManager alloc] initWithSessionConfiguration:configuration];
+    Sermon *sermon = [(SermonsDataSource *)self.tableView.dataSource sermonForIndexPath:indexPath];
+    NSURL *URL = [NSURL URLWithString:sermon.remoteUrlString];
+    NSURLRequest *request = [NSURLRequest requestWithURL:URL];
     
+    NSURLSessionDownloadTask *downloadTask = [manager downloadTaskWithRequest:request progress:nil destination:^NSURL *(NSURL *targetPath, NSURLResponse *response) {
+        NSURL *documentsDirectoryURL = [[NSFileManager defaultManager] URLForDirectory:NSDocumentDirectory inDomain:NSUserDomainMask appropriateForURL:nil create:NO error:nil];
+        return [documentsDirectoryURL URLByAppendingPathComponent:[response suggestedFilename]];
+    } completionHandler:^(NSURLResponse *response, NSURL *filePath, NSError *error) {
+        NSLog(@"File downloaded to: %@", filePath);
+    }];
+    [downloadTask resume];
 }
 #pragma mark - Segue
 -(void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
